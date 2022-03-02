bf3eb8da80da8a0364068e267110bc2207079f1e
ishaandwivedi1234 <ishaan.dwivedi99@gmail.com>
2022-03-03 00:59:37 +0530

    first

---
+++ js/main.js
@@ -0,0 +1,37 @@
+(function() {
+  "use strict";
+
+  /**
+   * Screenshot slider
+   */
+  new Swiper('.screenshot-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
+   * Animation on scroll
+   */
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', () => {
+    aos_init();
+  });
+
+})()
